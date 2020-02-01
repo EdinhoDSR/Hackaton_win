6e0f3b28524b5aa0f85de37906cf3f5a57f49023
Yakiigit <58605260+Yakiigit@users.noreply.github.com>
2020-02-01 14:23:21 +0100

    Add files via upload

--- CV.py
+++ CV.py
@@ -5,21 +5,39 @@ Created on Sat Feb  1 13:14:01 2020
 @author: Yakiimo
 """
 
+import os
+import csv
 from tika import parser
-raw = parser.from_file('1Amy.pdf')
-print(raw['content'])
-print("\n\n")
-text = raw['content'].lower().split()
+path = r"C:\Users\Yakiimo\Desktop\Hackathon CV\CV\Resume&Job_Description\Original_Resumes"
+dirs = os.listdir(path)
 
-excludedWords = ["or", "and", "with", "from", "of"]
+excludedWords = ["or", "and", "with", "from", "of", "any", "to", "for", "-", "by", "which", "as", "in", "on", "to", "–", "•", ":", "&"]
+keyWords = {}
 wordsCount = {}
 
-def scanCV(text) :
+def scanCV(dirs) :
+    for file in dirs :
+        if "pdf" in file :     
+            raw = parser.from_file(r"C:\Users\Yakiimo\Desktop\Hackathon CV\CV\Resume&Job_Description\Original_Resumes\\"+file)
+            print(raw['content'])
+            print("\n\n")
+            text = raw['content'].lower().split()
+            countWords(text)
+
+def countWords(text) :
     for word in text :
         if word in wordsCount.keys() :
             wordsCount[word]+=1
         else :
             wordsCount[word]=0
             
-scanCV(text)
+def updateKeyWords() :
+    for word in wordsCount.keys() :
+        if wordsCount[word] >= 30 and not word in excludedWords :
+            keyWords[word] = []
+            
+scanCV(dirs)
 print(wordsCount)
+updateKeyWords()
+print(keyWords)
+
