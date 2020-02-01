cfdafb28826743eaf9dcaa2053a3d56082e5e7c1
Edinhodsr <edinhodsr@gmail.com>
2020-02-01 15:06:31 +0100

    coco

--- CV.py
+++ CV.py
@@ -7,8 +7,9 @@ Created on Sat Feb  1 13:14:01 2020
 
 import os
 import csv
+from edinho import importance
 from tika import parser
-path = r"C:\Users\Yakiimo\Desktop\Hackathon CV\CV\Resume&Job_Description\Original_Resumes"
+path = r"CV\Resume&Job_Description\Original_Resumes"
 dirs = os.listdir(path)
 
 excludedWords = ["or", "and", "with", "from", "of", "any", "to", "for", "-", "by", "which", "as", "in", "on", "to", "–", "•", ":", "&"]
@@ -18,9 +19,9 @@ wordsCount = {}
 def scanCV(dirs) :
     for file in dirs :
         if "pdf" in file :     
-            raw = parser.from_file(r"C:\Users\Yakiimo\Desktop\Hackathon CV\CV\Resume&Job_Description\Original_Resumes\\"+file)
-            print(raw['content'])
-            print("\n\n")
+            raw = parser.from_file(r"CV\Resume&Job_Description\Original_Resumes\\"+file)
+            #print(raw['content'])
+            #print("\n\n")
             text = raw['content'].lower().split()
             countWords(text)
 
@@ -34,10 +35,11 @@ def countWords(text) :
 def updateKeyWords() :
     for word in wordsCount.keys() :
         if wordsCount[word] >= 30 and not word in excludedWords :
-            keyWords[word] = []
+            keyWords[word] = [wordsCount[word]]
             
 scanCV(dirs)
-print(wordsCount)
+#print(wordsCount)
 updateKeyWords()
-print(keyWords)
+print(importance(keyWords,r"CV\Resume&Job_Description\Original_Resumes\1Amy.pdf"))
+#print(keyWords)
 
--- edinho.py
+++ edinho.py
@@ -1,7 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb  1 12:23:50 2020
+Éditeur de Spyder
 
-@author: edinh
+Ceci est un script temporaire.
 """
+import os
+import math
+from tika import parser
 
+def importance(dico,CV):
+    
+    note=0
+    parsed = parser.from_file(CV)
+    CV_coupe = parsed["content"].split()
+    
+    for cle in dico.keys(): #pour chaque mots du dico des mots-clés, recuperer l'occurrrence dans le cv, le multiplier par ln(la valeur) et ajouter à la note
+        if dico.get(cle,0)[0] == 0 or CV_coupe.count(cle) ==0:
+            continue
+        note = note + math.log(CV_coupe.count(cle)*dico.get(cle,0)[0])
+    return note 
+
+#for element in parsed["content"]:
+#    print(element)
+#print(parsed["content"][0:50])
+"""
+for element in os.listdir('C:/Users/alexandre/Desktop/CV/Resume&Job_Description/Original_Resumes/Administration/CDIB HK - Office Manager') :
+    parsed = tika.parser.from_file('C:/Users/alexandre/Desktop/CV/Resume&Job_Description/Original_Resumes/Administration/CDIB HK - Office Manager/' + element)
+    print(parsed["content"])
+"""
+#help("parser")
